10ee851e23cf1f3eae1ce39da4ae5c8b8d8368db
kristi5491 <ksavchin05@gmail.com>
2023-09-15 15:24:30 +0300

    m

--- index2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Введіть перше число: "))
-num2 = int(input("Введіть друге число: "))
-
-print("Сума:", num1 + num2)
-print("Різниця:", num1 - num2)
-print("Ділення:", num1 / num2)
-print("Добуток:", num1 * num2)
-print("Рівність", num1 == num2)
