7696034e88bb9e76e2436e3bd6381ae80f3a3300
kristi5491 <ksavchin05@gmail.com>
2023-10-15 17:26:27 +0300

    final2

--- task5.py
+++ task5.py
@@ -1,12 +1,12 @@
 studets_file = 'students.csv'
 
 with open(studets_file, 'w', newline='') as file:
-    headers = ['Name ' 'LastName ' 'Age ' 'Specialty']
+    headers = ['id ' 'Name ' 'LastName ' 'Age ' 'Specialty']
     file.write(','.join(headers) + '\n')
 
-def add_students( name = '', lastname= '', age=0, specialty='' ):
+def add_students( id=0, name = '', lastname= '', age=0, specialty='' ):
     with open('students.csv', 'a', newline='') as file:
-        file.write(f'{name},{lastname},{age},{specialty}\n')
+        file.write(f'{id}, {name},{lastname},{age},{specialty}\n')
 
 
 def read_db():
@@ -14,22 +14,34 @@ def read_db():
         inf =  file.read()
         print(inf)
 
-def print_age(name = '', lastname= '', age=0, specialty=''):
-    with open(studets_file, 'r') as file:
-        lines = file.readlines()
-        for line in lines :
-            if age > 20:
-                print(f'{name}, {lastname}, {age}, {specialty}')
-            else:
-                print('---')
-        
+def print_age(id = 0, name = '', lastname= '', age=0, specialty=''):
+    with open(studets_file, 'r', newline='') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 5:
+                id_str, name, lastname, age_str, specialty = parts
+                try:
+                    id = int(id_str)
+                    age = int(age_str)
+                    if age >= 20:
+                        print(f'{id} {name}, {lastname}, {age}, {specialty}')
+                    else:
+                        print('---')
+                except ValueError:
+                    print('---')
+
+def rewrite_students(id=0, name = '', lastname= '', age=0, specialty=''):
+    with open(studets_file, 'w',newline='' ) as file:
+        id_choose = input('Введіть номер який хочете поміняти: ')
+        for line in file:
+            parts = line.strip().split(',')
 
 
 
 
-add_students('alica', 'wonk', 19, 'lawyer')
-add_students('Ola', 'Leins', 20, 'civil servant')
-add_students('lora', 'Jains', 21, 'Doctor')
-add_students('Andry' 'Mcalin', 19, 'Lawyer')
+add_students( 1, 'alica', 'wonk', 19, 'lawyer')
+add_students( 2, 'Ola', 'Leins', 20, 'civil servant')
+add_students( 3, 'lora', 'Jains', 21, 'Doctor')
+add_students( 4, 'Andry' 'Mcalin', 19, 'Lawyer')
 read_db()
 print_age()
