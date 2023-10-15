39d02b4c14bce9f4d2e20ab1b4127474d21e826b
kristi5491 <ksavchin05@gmail.com>
2023-10-15 17:25:51 +0300

    l2

--- task5.py
+++ task5.py
@@ -1,33 +1,23 @@
-import csv
-
-headers = ['Name ' 'LastName ' 'Age ' 'Specialty']
-# students = [['Ola', 'Leins', 20, 'civil servant'],
-#             ['Jhon', 'Mcarti', 22, 'Artist'],
-#             ['lora', 'Jains', 21, 'Doctor'],
-#              ['Andry' 'Mcalin', 19, 'Lawyer'] ]
-
 studets_file = 'students.csv'
 
 with open(studets_file, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
+    headers = ['Name ' 'LastName ' 'Age ' 'Specialty']
+    file.write(','.join(headers) + '\n')
+
 def add_students( name = '', lastname= '', age=0, specialty='' ):
-    with open(studets_file, 'a') as file:
-        writer = csv.writer(file)
-        students_d = [name, lastname, age, specialty]
-        writer.writerow(students_d)
+    with open('students.csv', 'a', newline='') as file:
+        file.write(f'{name},{lastname},{age},{specialty}\n')
+
+
 def read_db():
     with open(studets_file, 'r' ) as file:
-        inf = csv.reader(file)
-        for row in inf:
-            print(row)
+        inf =  file.read()
+        print(inf)
 
 def print_age(name = '', lastname= '', age=0, specialty=''):
     with open(studets_file, 'r') as file:
-        inf = csv.reader(file)
-        students_d = [name, lastname, age, specialty]
-        for row in inf:
-            age = int(age)
+        lines = file.readlines()
+        for line in lines :
             if age > 20:
                 print(f'{name}, {lastname}, {age}, {specialty}')
             else:
@@ -37,17 +27,9 @@ def print_age(name = '', lastname= '', age=0, specialty=''):
 
 
 
-
-
-
-
-
-
-
-
 add_students('alica', 'wonk', 19, 'lawyer')
 add_students('Ola', 'Leins', 20, 'civil servant')
 add_students('lora', 'Jains', 21, 'Doctor')
 add_students('Andry' 'Mcalin', 19, 'Lawyer')
-
+read_db()
 print_age()
