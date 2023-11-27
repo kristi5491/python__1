be8f7d8d38b536fc82c86bf02abf3dcf22b83136
kristi5491 <ksavchin05@gmail.com>
2023-11-27 22:40:02 +0200

    f1

--- main.py
+++ main.py
@@ -1,68 +1,52 @@
-import tkinter as tk
-from tkinter import scrolledtext
-import socket
-import threading
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+import pandas as pd
 
-window = tk.Tk()
-window.title("Chat")
-window.geometry("400x300")
+driver = webdriver.Safari()
 
-def send_message():
-    message = entry.get()
-    if message:
-        full_message = f"{name}: {message}"
-        client.send(full_message.encode())
-        chat_display.insert(tk.END, full_message + "\n")
-        entry.delete(0, tk.END)
-    else:
-       full_message = ""
-       client.send(full_message.encode()) 
+driver.get('https://www.pravda.com.ua/archives/')
+driver.maximize_window()
 
-def receive_messages():
-    while True:
-        try:
-            message = client.recv(1024).decode()
-            chat_display.insert(tk.END, message + "\n")
-        except ConnectionResetError:
-            break
+format_link = lambda day, month, year: f"https://www.pravda.com.ua/archives/date_{day}{month}{year}/"
 
-name = None
+df = pd.DataFrame(columns=['Date', 'Text'])
 
-def get_name():
-    global name
-    name = choose_name.get()
-    secondary_window.destroy()
-    send_message()
+def update_df(text, date):
+    global df
+    df = pd.concat([df, pd.DataFrame({'Date': date, 'Text': [text]})], ignore_index=True)
+    df.to_csv('pravda_data.csv', index=False)  
 
-secondary_window = tk.Toplevel(window)
-secondary_window.title("Choose name")
-secondary_window.geometry('240x60')
+def process_page_news(link, date):
+        article = []
+        driver.get(link)
 
-choose_name = tk.Entry(secondary_window, width=10)
-choose_name.grid(row=2, column=4, padx=12, pady=12, sticky="ew")
+        articles = driver.find_elements(By.CSS_SELECTOR, 'div.article.article_list a[href]')
 
-confirm_button = tk.Button(secondary_window, text='Confirm', width=5, command=get_name)
-confirm_button.grid(row=2, column=6, padx=12, pady=12, sticky="ew")
+        for article in articles:
+            try:
+                article_link = article.get_attribute('href')
+                if 'epravda.com.ua' in article_link or 'eurointegration.com.ua' in article_link or 'pravda.com.ua' in article_link:
+                    driver.get(article_link)
+                    main_div = driver.find_elements(By.CSS_SELECTOR, 'div.post__text p')
+                    text = [p.text for p in main_div]
+                    update_df(text, date)
+                elif 'life.pravda.com.ua' in article_link:
+                    driver.get(article_link)
+                    main_div = driver.find_elements(By.CSS_SELECTOR, 'article.article p')
+                    text = [p.text for p in main_div]
+                    update_df(text, date)
+                else:
+                    pass
+                driver.back()
+            except StaleElementReferenceException:
+                pass
+    
+for year in [2023]:
+    for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]:
+        process_page_news(format_link("02", month, year), f'02-{month}-{year}')
 
-chat_display = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=40, height=10)
-chat_display.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
-entry = tk.Entry(window, width=30)
-entry.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+input('wait')
 
-def send_button_press(event):
-    send_button.invoke()
-
-send_button = tk.Button(window, text="Send", command=send_message)
-send_button.grid(row=1, column=1, padx=10, pady=10, sticky="e")
-
-entry.bind('<Return>', send_button_press)
-
-window.columnconfigure(0, weight=1)
-window.rowconfigure(0, weight=1)
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 12345))
-server_thread = threading.Thread(target=receive_messages)
-server_thread.start()
-window.mainloop()
+driver.quit()
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def send_message():
-    message = entry.get()
-    if message:
-        chat_display.insert(tk.END, f"You: {message}\n")
-        entry.delete(0, tk.END) 
